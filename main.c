5a93eca8e2c1b7152c8590d8b9224a25ad62dcbc
Jvscopel33 <joao.s.ramos@edu.ufes.br>
2026-08-18 11:43:03 -0300

    add x

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 int main()
 {
-    int a;
+    int a, b;
     scanf("%d", &a);
     printf("Hello, world, your number is %d", a);
+    scanf("%d", &a);
+    printf("The another number is %d", b);
     return 0;
 }
